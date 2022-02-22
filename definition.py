b28d8b92e7534684fa4d9e83f5de882a42d2b543
nacho <ignacio.contrerascabello@gmail.com>
2022-02-22 07:56:27 +0100

    definition commited

---
+++ definition.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+class Definition:
+
+    def __init__(self, term):
+        self.term = term
+
+    def get(self):
+        df = pd.read_csv('data.csv')
+        return tuple(df.loc[df.word == self.term, 'definition'])
+
+if __name__ == '__main__':
+    d = Definition('rain')
+    print(d.get())
