3d53e753af6648f62fcc1c14f793f54c0e73c623
nacho <ignacio.contrerascabello@gmail.com>
2022-03-05 20:12:45 +0100

    Changed from button to fill

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -16,22 +16,17 @@ class Dictionary:
 
         input_div = jp.Div(a=div, classes='grid grid-cols-2')
 
-        input_box = jp.Input(a=input_div, placeholder='Type in a word here...',
+
+        output_div = jp.Div(a=div, classes='m-2 p-2 text-lg border-2 h-40')
+        input_box = jp.Input(a=input_div, placeholder='Type in a word here...', outputdiv=output_div,
                              classes='m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 focus:bg-white '
                                      'focus:outline-none focus:border-purple-500 py-2 px-4')
 
-        output_div = jp.Div(a=div, classes='m-2 p-2 text-lg border-2 h-40')
-
-        jp.Button(a=input_div,
-                  text='Get definition',
-                  classes='border-2 text-gray-500',
-                  click=cls.get_definition,
-                  outputdiv=output_div,
-                  inputbox=input_box)
+        input_box.on('input', cls.get_definition)
 
         return wp
 
     @staticmethod
     def get_definition(widget, msg):
-        defined = Definition(widget.inputbox.value).get()
+        defined = Definition(widget.value).get()
         widget.outputdiv.text = ' '.join(defined)
