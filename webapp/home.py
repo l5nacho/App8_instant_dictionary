6bbe9db6714b0d71bccf4ea809c44dfc25b5bdf5
nacho <ignacio.contrerascabello@gmail.com>
2022-03-05 20:43:01 +0100

    Home page

--- webapp/home.py
+++ webapp/home.py
@@ -1,11 +1,25 @@
 import justpy as jp
 
-class Home:
-    path = '/home'
 
-    def serve(self):
-        wb = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wb, classes='bg-gray-200 h-screen')
+class Home:
+    path = '/'
+
+    @classmethod
+    def serve(cls, req):
+        wp = jp.QuasarPage(tailwind=True)
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode='left', bordered=True)
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="Menu", click=cls.move_drawer,
+                drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text='Instant dictionary')
+        container = jp.QPageContainer(a=layout)
+
+        div = jp.Div(a=container, classes='bg-gray-200 h-screen p-2')
+
         jp.Div(a=div, text='This is the home page!', classes='text-4xl m-2')
         jp.Div(a=div, text='''
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
@@ -14,4 +28,11 @@ class Home:
             esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
             qui officia deserunt mollit anim id est laborum.
             ''', classes='text-lg')
-        return wb
+        return wp
+
+    @staticmethod
+    def move_drawer(widget, cls):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
